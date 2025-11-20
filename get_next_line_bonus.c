d2d3268f969d89d327a354ad6e581462df744c58
Caio Silva <csilva@c1r7s10.42lisboa.com>
2025-11-20 13:35:37 +0000

    Added new fixes

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: csilva <csilva@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/14 15:37:15 by csilva            #+#    #+#             */
-/*   Updated: 2025/11/20 13:29:29 by csilva           ###   ########.fr       */
+/*   Created: 2025/11/20 09:26:22 by csilva            #+#    #+#             */
+/*   Updated: 2025/11/20 13:31:56 by csilva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
+	static char	buffer[__FD_SETSIZE][BUFFER_SIZE + 1];
 	char		*line;
 	__ssize_t	byte_r;
 
@@ -23,32 +23,45 @@ char	*get_next_line(int fd)
 	line = NULL;
 	while (1)
 	{
-		if (buffer[0] == 0)
+		if (buffer[fd][0] == 0)
 		{
-			byte_r = read(fd, buffer, BUFFER_SIZE);
+			byte_r = read(fd, buffer[fd], BUFFER_SIZE);
 			if (byte_r == -1)
 				return (NULL);
 			if (byte_r == 0)
 				return (line);
 		}
-		line = ft_strjoin(line, buffer);
+		line = ft_strjoin(line, buffer[fd]);
 		if (!line)
 			return (NULL);
-		updt_buffer(buffer);
+		updt_buffer(buffer[fd]);
 		if (ft_strchr(line, '\n'))
 			return (line);
 	}
 }
 
-/* int	main(void)
+int	main(void)
 {
 	char	*res;
 	int		i;
+	int		j;
+	int		k;
 
-	i = open("test.txt", O_RDONLY);
+	i = open("test1.txt", O_RDONLY);
 	res = get_next_line(i);
-	printf("%s", res);
+	printf("%s\n", res);
 	free(res);
+
+	j = open("test2.txt", O_RDONLY);
+	res = get_next_line(j);
+	printf("%s\n", res);
+	free(res);
+
+	k = open("test3.txt", O_RDONLY);
+	res = get_next_line(k);
+	printf("%s\n", res);
+	free(res);
+
 	close(i);
 
-} */
+}
